fd200bd697045f5e334a32b7de300f03b9d04121
Edrichthedev <136769361+Edrichthedev@users.noreply.github.com>
2023-06-16 12:25:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 from PIL import Image 
 import requests
 import streamlit as st
-from streamlit_lottie import st.lottie
+from streamlit_lottie import st_lottie
 with st.echo():
     st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
 
